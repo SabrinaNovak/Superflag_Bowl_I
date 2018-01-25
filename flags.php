ad2ec122af040e849060208f708ac49bf9451580
Sabrina Novak <nosabrina@icloud.com>
2018-01-25 10:58:52 +0100

    flags.php now returns valid JSON data

--- flags.php
+++ flags.php
@@ -7,6 +7,21 @@ use HTL3R\Flags\Flags\TriangleFlag as TriangleFlag;
 use HTL3R\Flags\Flags\RectangleFlag as RectangleFlag;
 
 
-$myFlag[] = new RectangleFlag("England", 24.5, 2.0, 0.5, "#F00");
-$myFlag[] = new TriangleFlag("Barbados Pirates", 32.5, 4.0, 0.2, "#00F");
-$myFlag[] = new RectangleFlag("Spanien", 20.5, 2.0, 0.5, "#F0F");
+$myFlags[] = new RectangleFlag("England", 24.5, 2.0, 0.5, "#F00");
+$myFlags[] = new TriangleFlag("Barbados Pirates", 32.5, 4.0, 0.2, "#00F");
+$myFlags[] = new RectangleFlag("Spanien", 20.5, 2.0, 0.5, "#F0F");
+
+header('Content-Type: application/json');
+
+// because flag properties are protected - this next step is necessary
+foreach ($myFlags as $flag) {
+    $flagSortiment[] = [
+        "name" => $flag->getName(),
+        "price" => $flag->getPrice(),
+        "width" => $flag->getWidth(),
+        "height" => $flag->getHeight(),
+        "color" => $flag->getColor()
+    ];
+}
+
+die(json_encode($flagSortiment));
