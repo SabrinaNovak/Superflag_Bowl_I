c0b9efecb9e385c4d07b878748d2ad094b57bed2
Sabrina Novak <nosabrina@icloud.com>
2018-01-26 01:36:51 +0100

    required components/flag-icon-css, flag langcode fixes

--- flags.php
+++ flags.php
@@ -7,9 +7,9 @@ use HTL3R\Flags\Flags\TriangleFlag as TriangleFlag;
 use HTL3R\Flags\Flags\RectangleFlag as RectangleFlag;
 
 
-$myFlags[] = new RectangleFlag("England", 24.5, 2.0, 0.5, "#F00");
-$myFlags[] = new TriangleFlag("Barbados Pirates", 32.5, 4.0, 0.2, "#00F");
-$myFlags[] = new RectangleFlag("Spanien", 20.5, 2.0, 0.5, "#F0F");
+$myFlags[] = new RectangleFlag("England", 24.5, 2.0, 0.5, "#F00", "GB");
+$myFlags[] = new TriangleFlag("Cocos Islands", 32.5, 4.0, 0.2, "#00F", "CC");
+$myFlags[] = new RectangleFlag("Spanien", 20.5, 2.0, 0.5, "#F0F", "ES");
 
 // because flag properties are protected - this next step is necessary
 foreach ($myFlags as $flag) {
@@ -18,7 +18,8 @@ foreach ($myFlags as $flag) {
         "price" => $flag->getPrice(),
         "width" => $flag->getWidth(),
         "height" => $flag->getHeight(),
-        "color" => $flag->getColor()
+        "color" => $flag->getColor(),
+        "langcode" => $flag->getLangcode()
     ];
 }
 
